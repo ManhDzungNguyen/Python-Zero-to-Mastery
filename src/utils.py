07b0b92146bb9896c5218e6f4fd71a3741652c0f
dungnm <nguyenmanhdzung5920@gmail.com>
2023-01-08 17:32:15 +0700

    update src

---
+++ src/utils.py
@@ -0,0 +1,10 @@
+print(__name__)
+
+def print_christmas_tree():
+    print("    *     ")
+    print("   ***    ")
+    print("  *****   ")
+    print(" *******  ")
+    print("********* ")
+    print("    |     ")
+    print("    |     ")
