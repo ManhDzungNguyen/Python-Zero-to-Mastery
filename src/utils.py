7e097641314f2855c60faf92281c1a8acedf967f
dungnm <dungnm@sendo.vn>
2023-01-09 09:07:38 +0700

    update regex

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
-print(__name__)
+from functools import wraps
+
 
 def print_christmas_tree():
     print("    *     ")
@@ -8,3 +9,29 @@ def print_christmas_tree():
     print("********* ")
     print("    |     ")
     print("    |     ")
+
+
+from functools import wraps
+
+
+class SuperPrint:
+    def __init__(self) -> None:
+        def idx():
+            i = 0
+            while True:
+                yield i
+                i += 1
+
+        self.id = idx()
+
+    def _index(func):
+        @wraps(func)
+        def wrapper(self, *args, **kwargs):
+            print(self.id.__next__(), end="  ")
+            func(self, *args, **kwargs)
+
+        return wrapper
+
+    @_index
+    def print_idx(self, *values):
+        print(*values)
